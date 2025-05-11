cd3de505cc10b83d76adbf75bc22c541bff6c844
nik2225 <nikbhamare0410@gmail.com>
2025-05-12 00:32:59 +0530

    github_action

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,12 @@
+from app import app
+
 def test_home():
-    from app import app
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
+
+def test_register():
+    client = app.test_client()
+    response = client.post('/register', data={'name': 'Nikhil', 'email': 'nikhil@example.com'})
+    assert response.status_code == 200
+    assert b"Registered Nikhil with email nikhil@example.com" in response.data
